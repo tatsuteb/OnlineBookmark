463df2399c83bf9480064828573309c1c911aaaf
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2018-09-18 00:45:35 +0900

    ブックマークを登録するコントローラとビューを用意

---
+++ OnlineBookmark/Controllers/BookmarksController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookmark.Models.Bookmarks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineBookmark.Controllers
+{
+    [Authorize]
+    public class BookmarksController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateBookmark(BookmarkCreationModel model, string returnUrl)
+        {
+            Console.WriteLine("Create Bookmark.");
+
+            if (string.IsNullOrEmpty(returnUrl))
+                return RedirectToAction("Index", "Top");
+
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ OnlineBookmark/Models/Bookmarks/BookmarkCreationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace OnlineBookmark.Models.Bookmarks
+{
+    public class BookmarkCreationModel
+    {
+        public IFormFile ImageFile { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
